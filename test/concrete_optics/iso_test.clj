a50017919943af767bf7035639361c77ffa58357
Sonat Suer <sonatsuer@gmail.com>
2023-05-12 00:48:05 +0200

    Add tests for curried iso

--- test/concrete_optics/iso_test.clj
+++ test/concrete_optics/iso_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
+            [concrete-optics.test-helpers :refer [typed-eq compare-functions]]
             [concrete-optics.iso.structures :refer :all]
             [concrete-optics.iso.axioms :refer :all]))
 
@@ -12,3 +13,19 @@
 (defspec eq-review-view-test 100
   (prop/for-all [x gen/any-equatable]
                 (review-view-axiom eq x)))
+
+(defspec curried-view-review-test 1 
+  (let [f (fn [x y] {:x x :y y})
+        sample-size 500
+        generator (gen/vector gen/any-equatable sample-size)]
+    (prop/for-all [xs generator
+                   ys generator]
+                  (view-review-axiom curried f (compare-functions typed-eq xs ys)))))
+
+(defspec curried-review-view-axiom-test 1
+  (let [f (fn [x] (fn [y] {:x x :y y}))
+        sample-size 1
+        generator (gen/vector gen/any-equatable sample-size)]
+    (prop/for-all [fs (gen/fmap (fn [vec] (into [] (map f vec))) generator)
+                   xs generator]
+                  (review-view-axiom curried f (compare-functions (compare-functions typed-eq xs) fs)))))
